99e024dcda0d604c95d6b87c5c96f9baa4823ccc
lenanetka <lenapaley@gmail.com>
2024-05-25 00:52:58 +0200

    Generate result

--- report/ReportGenerator.js
+++ report/ReportGenerator.js
@@ -7,36 +7,54 @@ export default class ReportGenerator {
     }
     async generate(name) {
         this.report = new Report(name);
-        
         let tabConfigs = Config.report;
         for (const tabConfig of tabConfigs) {
             this.report.addTab(tabConfig.name);
-            this.report.addResult(await this.calculateResult(tabConfig));
+            this.report.addResult(await this.generateResult(tabConfig));
         }
     }
-    async calculateResult(tabConfig){
+    async generateResult(tabConfig) {
         let result = [];
-        let testRailAPI = new TestRailAPI();
-        let testCases = await testRailAPI.getTestCases(tabConfig.project_id, tabConfig.suite_id, tabConfig.filters);
-        testCases = testCases.sort((a, b) => a[tabConfig.group_by].localeCompare(b[tabConfig.group_by]));
-        let groupName;
+        let testCases = await this.getTestCases(tabConfig);
         result.push(tabConfig.columns);
+        let groupName;
+        let sectionName;
         for (const testCase of testCases) {
-            if(testCase[tabConfig.group_by] != groupName){
+            if (tabConfig.show_groups && testCase[tabConfig.group_by] != groupName) {
                 groupName = testCase[tabConfig.group_by];
-                result.push([null, groupName]);
+                let groupLine = [null, groupName];
+                result.push(groupLine);
             }
-            let line = this.calculateLine(testCase, tabConfig.columns);
+            if (testCase.section.depth < tabConfig.sections_max_depth && testCase.section.name != sectionName) {
+                sectionName = testCase.section.name;
+                let sectionLine = [null, sectionName];
+                if (JSON.stringify(result[result.length - 1]) != JSON.stringify(sectionLine)) result.push(sectionLine);
+            }
+            let line = this.generateLine(testCase, tabConfig.columns);
             result.push(line);
         }
         return result;
     }
-    calculateLine(testCase, columns){
+    generateLine(testCase, columns) {
         let line = [];
         for (const column of columns) {
             line.push(testCase[column]);
         }
         return line;
+    }
+    async getTestCases(tabConfig) {
+        let testRailAPI = new TestRailAPI();
+        let testCases = await testRailAPI.getTestCases(tabConfig.project_id, tabConfig.suite_id, tabConfig.filters);
+        testCases = testCases.sort((a, b) => a[tabConfig.group_by].localeCompare(b[tabConfig.group_by]));
+        let sections = await testRailAPI.getSections(tabConfig.project_id, tabConfig.suite_id);
+        testCases = this.addSectionsInfoToTestCases(testCases, sections);
+        return testCases;
+    }
+    addSectionsInfoToTestCases(testCases, sections) {
+        for (let i = 0; i < testCases.length; i++) {
+            testCases[i].section = sections.find(x => x.id === testCases[i].section_id);
+        }
+        return testCases;
     }
     saveToXLSX() {
         let reportWriterXLSX = new ReportWriterXLSX(this.report.name);
